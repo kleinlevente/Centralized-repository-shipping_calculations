ed26de32fa13729e3cf8ad48b2e5cc6b90311885
Levente Klein <kleinl@us.ibm.com>
2024-08-14 18:41:00 -0400

    Revert "addded a new line"
    This reverts commit 6e9babfe13e625991e1ced5b4646b2a506617c22.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,5 @@
 # Shipping Cost Calculator
  # Here is a new update by kleinlevente2
-
-# Here is another update by leventeklein22
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
